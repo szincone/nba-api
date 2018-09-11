efe9ed9c940b419187d9c3659894422d00a3319c
szincone <sawyerzincone@gmail.com>
2018-09-11 01:33:14 -0600

    added some cheerio boilerplate that gets rough table

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -4,12 +4,21 @@ const cheerio = require("cheerio");
 request(
   "https://www.basketball-reference.com/leagues/NBA_2017_per_game.html",
   (error, res, html) => {
+    let playerTable = [];
     if (!error && res.statusCode === 200) {
       const $ = cheerio.load(html);
-      const table = $("tbody")
-        .children("tr")
-        .text();
-      console.log(table);
+      // const table = $("tbody")
+      //   .children("tr")
+      //   .text();
+      // console.log(table);
+      $("tbody").each((i, el) => {
+        playerTable.push(
+          $(el)
+            .find("tr")
+            .text(),
+        );
+        console.log(playerTable);
+      });
     }
   },
 );
