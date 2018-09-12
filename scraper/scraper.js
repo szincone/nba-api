ab56aac2f6a92d43ea392ea7b6b969ff047f2b78
szincone <sawyerzincone@gmail.com>
2018-09-11 18:12:46 -0600

    rough version of parsed table loading

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,24 +1,21 @@
 const request = require("request");
 const cheerio = require("cheerio");
-
+const cheerioTableparser = require("cheerio-tableparser");
 request(
   "https://www.basketball-reference.com/leagues/NBA_2017_per_game.html",
   (error, res, html) => {
     let playerTable = [];
     if (!error && res.statusCode === 200) {
-      const $ = cheerio.load(html);
+      $ = cheerio.load(html);
       // const table = $("tbody")
       //   .children("tr")
       //   .text();
       // console.log(table);
-      $("tbody").each((i, el) => {
-        playerTable.push(
-          $(el)
-            .find("tr")
-            .text(),
-        );
-        console.log(playerTable);
-      });
+
+      $("table").text();
+      cheerioTableparser($);
+      playerTable = $("table").parsetable(true, true, true);
+      console.log(playerTable);
     }
   },
 );
