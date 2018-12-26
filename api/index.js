ec155c5c9e73021dacccddfc1f99df5861a30ed8
szincone <sawyerzincone@gmail.com>
2018-12-25 17:12:57 -0700

    imported .env port variable

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 "use strict";
+require("dotenv").config();
 const express = require("express");
 const server = new express();
 
@@ -6,7 +7,7 @@ const server = new express();
 const configureMiddleware = require("./middleware/middleware.js");
 configureMiddleware(server);
 
-const PORT = 9000;
+const PORT = process.env.PORT || 9000;
 
 server.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
