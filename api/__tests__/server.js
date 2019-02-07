32c434c963a2ed27c3bbbec49337ac0a930b31ae
szincone <sawyerzincone@gmail.com>
2019-02-07 15:44:08 -0700

    added GET all suite :white_check_mark: - also added 200 status code test to suite

--- api/__tests__/server.js
+++ api/__tests__/server.js
@@ -1,13 +1,20 @@
 const request = require('supertest');
 const express = require('express');
+const configureMiddleware = require('../middleware/middleware.js');
 
 const server = express();
-const configureMiddleware = require('../middleware/middleware.js');
 
 configureMiddleware(server);
 
 describe('server.js', () => {
   test('runs the tests', () => {
     expect(true).toBeTruthy();
+  });
+
+  describe('GET /', () => {
+    test('returns a 200 (OK) status code', async () => {
+      const response = await request(server).get('/api/players');
+      expect(response.status).toEqual(200);
+    });
   });
 });
