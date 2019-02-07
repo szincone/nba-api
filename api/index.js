0f6197915e0a0b5027f2800a66a139c47f20c197
szincone <sawyerzincone@gmail.com>
2019-02-07 15:34:58 -0700

    refactored for eslint :recycle:

--- api/index.js
+++ api/index.js
@@ -1,10 +1,8 @@
 require('dotenv').config();
 const express = require('express');
+const configureMiddleware = require('./middleware/middleware.js');
 
 const server = express();
-
-// middleware
-const configureMiddleware = require('./middleware/middleware.js');
 
 configureMiddleware(server);
 
