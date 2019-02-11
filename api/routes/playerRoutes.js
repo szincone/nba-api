551fb04b27112c0e15714d72f80568b0f9a55057
szincone <sawyerzincone@gmail.com>
2019-02-11 00:58:53 -0700

    rewrote routes using async/await :recycle:

--- api/routes/playerRoutes.js
+++ api/routes/playerRoutes.js
@@ -3,29 +3,25 @@ const helpers = require('../db/dbHelper/helpers.js');
 
 const router = express.Router();
 
-// start get requests
-router.get('/', (req, res, next) => {
-  helpers
-    .getPlayers()
-    .then((players) => {
-      res.status(200).json(players);
-    })
-    .catch((err) => {
-      next(err);
-    });
+// gets
+router.get('/', async (req, res, next) => {
+  try {
+    const players = await helpers.getPlayers();
+    res.status(200).json(players);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   const { id } = req.params;
-  helpers
-    .getPlayer(id)
-    .then((player) => {
-      res.status(200).json(player);
-    })
-    .catch((err) => {
-      next(err);
-    });
+  try {
+    const player = await helpers.getPlayer(id);
+    res.status(200).json(player);
+  } catch (err) {
+    next(err);
+  }
 });
-// end get requests
+// end gets
 
 module.exports = router;
