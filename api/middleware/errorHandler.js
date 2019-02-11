384c16be9cc7fe01138b126f49370ea2e8cad3b5
szincone <sawyerzincone@gmail.com>
2019-02-11 01:57:20 -0700

    updated err handler :recycle: - better messages

--- api/middleware/errorHandler.js
+++ api/middleware/errorHandler.js
@@ -1,25 +1,24 @@
-function errorHandler(err, req, res) {
+function errorHandler(err, req, res, next) {
   switch (err.code) {
-    case 404:
-      res.status(404).json({
-        message: 'The requested post does not exist.',
-      });
-      break;
     case 400:
       res.status(400).json({
-        message: 'There was an error regarding your input.',
+        message: 'Request cannot be understood or processed.',
+      });
+      break;
+    case 404:
+      res.status(404).json({
+        message: 'The player cannot be found.',
       });
       break;
     case 406:
       res.status(406).json({
-        message: 'Missing title or content.',
+        message: 'Missing one or more required fields.',
       });
       break;
     default:
       res.status(500).json({
-        message: 'There was an error performing the required operation',
+        message: 'There was an error while performing this operation.',
       });
-      break;
   }
 }
 
