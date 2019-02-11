da68feba70a5d5a923f58b9f1ea79f518f855a5d
szincone <sawyerzincone@gmail.com>
2019-02-11 02:07:43 -0700

    refactored to use res status obj :recycle:

--- api/middleware/errorHandler.js
+++ api/middleware/errorHandler.js
@@ -1,22 +1,24 @@
+const { responseStatus } = require('../routes/responseStatus.js');
+
 function errorHandler(err, req, res, next) {
   switch (err.code) {
-    case 400:
-      res.status(400).json({
+    case responseStatus.badRequest:
+      res.status(responseStatus.badRequest).json({
         message: 'Request cannot be understood or processed.',
       });
       break;
-    case 404:
-      res.status(404).json({
+    case responseStatus.notFound:
+      res.status(responseStatus.notFound).json({
         message: 'The player cannot be found.',
       });
       break;
-    case 406:
-      res.status(406).json({
+    case responseStatus.notAcceptable:
+      res.status(responseStatus.notAcceptable).json({
         message: 'Missing one or more required fields.',
       });
       break;
     default:
-      res.status(500).json({
+      res.status(responseStatus.internalErr).json({
         message: 'There was an error while performing this operation.',
       });
   }
