ebb0ea7f459c4400db83599e1345d371790a3597
szincone <sawyerzincone@gmail.com>
2019-02-11 01:56:30 -0700

    added status code obj :speech_balloon:

---
+++ api/routes/responseStatus.js
@@ -0,0 +1,20 @@
+// module.exports = {
+//   responseStatus: {
+//     success: 200,
+//     badRequest: 400,
+//     notFound: 404,
+//     notAcceptable: 406,
+//     internalErr: 500,
+//   },
+// };
+const responseStatus = {
+  success: 200,
+  badRequest: 400,
+  notFound: 404,
+  notAcceptable: 406,
+  internalErr: 500,
+};
+
+module.exports = {
+  responseStatus,
+};
